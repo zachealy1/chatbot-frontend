33b57a8ca6567f9abd6a8ba19c055a023cac5be3
Zac Healy <zac.healy@hotmail.co.uk>
2025-01-02 21:45:11 +0100

    govuk-frontend 4.9.0 -> 5.7.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const sourcePath = path.resolve(__dirname, 'src/main/assets/js');
 const govukFrontend = require(path.resolve(__dirname, 'webpack/govukFrontend'));
 const scss = require(path.resolve(__dirname, 'webpack/scss'));
 const HtmlWebpack = require(path.resolve(__dirname, 'webpack/htmlWebpack'));
 
+const govukMacrosPath = path.dirname(
+  require.resolve('govuk-frontend/dist/govuk/macros/attributes.njk')
+);
+
 const devMode = process.env.NODE_ENV !== 'production';
 const fileNameSuffix = devMode ? '-dev' : '.[contenthash]';
 const filename = `[name]${fileNameSuffix}.js`;
 
 module.exports = {
-  plugins: [...govukFrontend.plugins, ...scss.plugins, ...HtmlWebpack.plugins],
+  plugins: [
+    ...govukFrontend.plugins,
+    ...scss.plugins,
+    ...HtmlWebpack.plugins,
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: govukMacrosPath,
+          to: path.resolve(__dirname, 'src/main/views/govuk/macros'),
+        },
+      ],
+    }),
+  ],
   entry: path.resolve(sourcePath, 'index.ts'),
   mode: devMode ? 'development' : 'production',
   module: {
